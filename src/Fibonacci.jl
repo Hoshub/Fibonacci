006d00e539d5dd31ece8a3f3329c19919bf14a02
test-250626-1612 <2064158879@qq.com>
2026-01-14 15:39:30 +0800

    Initial commit

---
+++ src/Fibonacci.jl
@@ -0,0 +1,35 @@
+module Fibonacci
+
+# 让别人用到这个函数
+export calculate_fibonacci, is_prime
+
+function calculate_fibonacci(n::Int64)
+    if n <= 0
+        return 0
+    elseif n <= 2
+        return 1
+    end
+    a, b = 1, 1
+
+    # 迭代
+    for i in 3:n
+        a, b = b, a + b
+    end
+    return b
+end
+
+function is_prime(n::int)
+
+    if n <= 1
+        return false
+    end
+
+    for i in 2:isqrt(n)
+        if n % i == 0
+            return false
+        end
+    end
+    return true
+end
+
+end # module Fibonacci
